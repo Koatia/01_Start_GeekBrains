41d4414def0c842d91c2e442ddcaf3c240afdd59
Konstantin <yarohno@gmail.com>
2024-06-13 20:40:34 +0300

    modified:   gitPush.sh

--- gitPush.sh
+++ gitPush.sh
@@ -1,4 +1,10 @@
+# Добавление удаленных репозиториев
+git remote add origin git@github.com:Koatia/01_Start_GeekBrains.git;
+git remote add mirror ssh://git@gitverse.ru:2222/Kostia/01_Start_GeekBrains.git
+
+# Пуш изменений в оба репозитория
 git push origin master; git push mirror master
+
 echo '--------'
 echo '--------'
 git status
